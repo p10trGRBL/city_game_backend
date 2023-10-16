835b4909b7a90a30c91207fd4faf1b420b16eed2
KateOblomova <kate.oblom@gmail.com>
2023-10-16 12:06:53 +0200

    user backend

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import express from 'express';
-import cors from 'cors';
-import {ErrorHanlder} from './middleware/ErrorHandler.js';
-import cityRoutes from './routes/cityRoutes.js';
-import './db/server.js';
+import express from "express";
+import cors from "cors";
+import { ErrorHanlder } from "./middleware/ErrorHandler.js";
+import cityRoutes from "./routes/cityRoutes.js";
+import "./db/server.js";
+
+const users = require("./routes/User");
+
+app.use(express.urlencoded({ extended: true }));
 
 const app = express();
-const PORT = process.env.PORT ||8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
 app.use(ErrorHanlder);
 
-app.listen(PORT, ()=>console.log(`Server is listening to ${PORT}`))
+app.use("/leaderboard/users", users);
 
-
+app.listen(PORT, () => console.log(`Server is listening to ${PORT}`));
