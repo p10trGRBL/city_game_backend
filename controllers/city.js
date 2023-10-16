3ad302d114e74d8e5096150eb84d935b27b1b1f6
KateOblomova <kate.oblom@gmail.com>
2023-10-16 14:18:27 +0200

    city get all

--- controllers/city.js
+++ controllers/city.js
@@ -1,17 +1,30 @@
-const City = require ('../models/City.js');
+const City = require("../models/City.js");
 
-
- const getCityById = async(req, res, next) => {
-    const {id} = req.params;
-    try {
-        const city = await City.findById(id);
-        if (!city) {
-            throw {statusCode: 404, message: 'City not found'}
-        }
-        res.send(city);
-    } catch (error) {
-        next(error);
+const getCityById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const city = await City.findById(id);
+    if (!city) {
+      throw { statusCode: 404, message: "City not found" };
     }
+    res.send(city);
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports = {getCityById};
+const getAllCities = async (req, res) => {
+  try {
+    const cities = await City.find();
+    if (!cities.length) {
+      res.status(200).json({ msg: "No city found" });
+    } else {
+      res.status(200).json({ cities });
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Failed to find city data" });
+    console.log(err);
+  }
+};
+
+module.exports = { getCityById, getAllCities };
--- routes/cityRoutes.js
+++ routes/cityRoutes.js
@@ -1,10 +1,10 @@
-const express = require ('express');
+const express = require("express");
 
-const { getCityById } =require ( '../controllers/city.js');
+const { getCityById, getAllCities } = require("../controllers/city.js");
 
 const cityRouter = express.Router();
 
-cityRouter.route('/:id').get(getCityById);
-
+cityRouter.route("/:id").get(getCityById);
+cityRouter.route("/").get(getAllCities);
 
 module.exports = cityRouter;
